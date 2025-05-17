cbee655938e8e9bc576e9b2fdaba8347e4b94cce
School-account-10 <160393316+School-account-10@users.noreply.github.com>
2025-05-17 12:18:37 +0800

    newest

---
+++ app/src/main/java/com/example/tutorial/Login.java
@@ -0,0 +1,37 @@
+package com.example.tutorial;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Button btq = (findViewById(R.id.Login));
+        btq.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View btq) {
+                Intent gs = new Intent(Login.this, mainpage.class);
+                startActivity(gs);
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/example/tutorial/mainpage.java
@@ -0,0 +1,40 @@
+package com.example.tutorial;
+
+import static androidx.core.content.ContextCompat.startActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.os.Handler;
+import android.os.Looper;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class mainpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_mainpage);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent gs = new Intent(mainpage.this, Final.class);
+                startActivity(gs);
+            }
+        }, 3000);
+    }
+}
