4057ce5fcf48d085840e1c0935aa175057eaf8e7
School-account-10 <160393316+School-account-10@users.noreply.github.com>
2025-05-27 22:42:40 +0800

    fgf

--- app/src/main/java/com/example/moodify/MainActivity2.java
+++ app/src/main/java/com/example/moodify/MainActivity2.java
@@ -1,9 +1,6 @@
 package com.example.moodify;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,27 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-
-        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-            @Override
-            public void run() {
-
-                Intent gs = new Intent(MainActivity.this, MainActivity2.class);
-                startActivity(gs);
-            }
-        }, 3000);
     }
 }
